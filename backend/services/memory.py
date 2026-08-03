c0f27bcdbe865cbf0e923c92bd62f3eb5424d0d4
manideepreddy <manideepreddytippana@gmail.com>
2026-08-04 00:16:26 +0530

    configured memory for conversations,messages

---
+++ backend/services/memory.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Conversation, Message
+
+class Memory:
+    def __init__(self, max_turns: int = 5):
+        self.message_limit = max_turns * 2
+
+    def get_history(self, session_id: str):
+
+        with SessionLocal() as db:
+            conversation = db.query(Conversation).filter(Conversation.session_id == session_id).first()
+
+            if conversation:
+                recent_messages = conversation.messages[-self.message_limit:]
+                return recent_messages
+            return ""
+
+            history = []
+    
+            for message in recent_messages:
+    
+                role = "User" if message.role == "user" else "Assistant"
+                history.append(f"{role}: {message.content}")
+            return "\n".join(history)
+
+    def add_message(self, session_id: str, user_message: str, assistant_message: str):
+
+        with SessionLocal() as db:
+
+            try:
+                conversation = db.query(Conversation).filter(Conversation.session_id == session_id).first()
+    
+                if not conversation:
+                    conversation = Conversation(session_id=session_id)
+                    db.add(conversation)
+                    db.flush()
+    
+                user_msg = Message(conversation_id=conversation.id, role="user", content=user_message)
+                assistant_msg = Message(conversation_id=conversation.id, role="assistant", content=assistant_message)
+                db.add_all([user_msg, assistant_msg])
+                db.commit()
+
+            except Exception:
+                db.rollback()
+                raise "Failed to interract with Postgresql DB"
+
+                
